e5c0998520ec8f62659bcf60ad7575cbf7f98d78
Yub_2 <hyeok230@naver.com>
2022-06-24 20:34:40 +0900

    test:git test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: byahn <byahn@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 16:28:09 by byahn             #+#    #+#             */
-/*   Updated: 2022/06/21 17:21:18 by byahn            ###   ########.fr       */
+/*   Updated: 2022/06/24 20:33:33 by junylee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ static void init(int argc, char *argv[], char *envp[])
 int main(int argc, char *argv[], char *envp[])
 {
 	init(argc, argv, envp);
-
+	(void)envp;
 	while(1)
 	{
-		
+			
 	}
 	return (0);
 }
