dae062f26b971b80b64f02d0b5863dc84c76d34d
nonoaa <nonoaa1@naver.com>
2022-06-24 19:57:59 +0900

    init

---
+++ main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byahn <byahn@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 16:28:09 by byahn             #+#    #+#             */
+/*   Updated: 2022/06/21 17:21:18 by byahn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void init(int argc, char *argv[], char *envp[])
+{
+
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+	init(argc, argv, envp);
+
+	while(1)
+	{
+		
+	}
+	return (0);
+}
